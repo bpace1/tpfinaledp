365bc42fd8624a57a594d3153f27eb6e196825ba
Alumno <brunopace2001@gmail.com>
2023-06-16 20:49:19 -0300

    Se avanzó con el archivo

--- clase16jun/menu.sh
+++ clase16jun/menu.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-read CMD
+
 menu (){
 	echo "Ingrese un comando de la siguiente lista:"
 	echo "Descargar"
 	echo "Generar"
 	echo "Procesar"
 	echo "Comprimir"
-	if [[ CMD  =~ +[A-Ba-B]$ ]] ; then
-		case expresion in
+	read CMD
+	if [[ $CMD  =~ +[A-Ba-B]$ ]] ; then
+		case $CMD in
 			descargar)
 				echo "Se descargarán las imágenes de la web"
-				descargar.sh #ver como ejecutar otro archivo .sh dentro de uno.
+				#descargar.sh #ver como ejecutar otro archivo .sh dentro de uno.
 			;;
 			generar)
 				echo "Se están generando las imágenes"
-				generar.sh
+				#generar.sh
 			;;
 			procesar)
 				echo "Se están procesando las imágenes"
+				#procesar.sh
 			;;
 			comprimir)
 				echo "Se están comprimiendo los archivos"
-				comprimir.sh 
+				#comprimir.sh 
+			;;
 		esac
 	else 
 		echo "No es un comando valido"
 	fi
 
 }
+
+menu
