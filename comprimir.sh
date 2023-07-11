3daf402e63cac57a56aaa2ff9a8df3f99a60a14a
Bruno <brunopace2001@gmail.com>
2023-07-10 23:08:39 -0300

    fin de archivo comprimir.sh se da fin al archivo comprimir.sh

--- comprimir.sh
+++ comprimir.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+#init: none -> string string
+#marca el inicio de este archivo mediante la repeticion de guiones medios.
+init(){
+        echo -------------------------------------------------------
+        echo "Se iniciará el proceso del archivo comprimir"
+
+}
+
 
 #getNames: none -> file string
 #se encarga de hacer la lista de los nombres que toman las imagenes en un archivo listNames
 getNames(){
-	ls dir > listNames
+	mkdir -p dir2/
+	ls dir > dir2/listNames
 	echo "Archivo de todos los nombres de imágenes generado."
 }
 
 #getValidNames: none -> file string
 #se encargade hacer la lista de los nombres validos que toman las imagenes en un archivo listValidNames
 getValidNames(){
-	ls dir2 > listValidNames
+	ls dir2 > dir2/listValidNames
 	echo "Archivo de nombres validos de imágenes generado"
 
 }
@@ -19,24 +28,44 @@ getValidNames(){
 #endingCharA:
 #genera un archivo del total de personas que cuyo nombre finaliza con la letra "a"
 endingCharA(){
-	while IFS= read -r line
-	do
-	  echo "$line"
-	  nombre=$($line|cut -d " " -f-1)
-	  echo $nombre
-	  length=$(echo {$name})
-	  echo "$lenght"
-	  # if [[name == a ]] 
-	done < listNames
-	#lines=$(wc -l listNames)
-	#for ((i=1, i<=$lines, i++ )); do
-	#	lastchar=$(cut -d " " -f-1)
-	#	if [[ ]]
-
+	echo "Iniciando chequeo de nombres finalizados en 'a' "
+	while IFS= read -r name
+	do 
+	  lenName=${#name}
+	  lastChar=${name:lenName-1:1}
+	   if [[  $lastChar == "a" ]] ; then
+		   echo  $name >> dir2/endingWA
+	   fi
+	done < dir2/listNames
+	echo "Chequeo terminado con éxito"
+	echo "En caso de corresponder, el archivo fue creado"
 }
 
 
-endingCharA
+#compress: none -> string
+#comprime un archivo.
+compress(){
+        tar -zcf comprimidoFinal.tar.gz  dir/ dir2/
+        echo "Se comprimió el archivo."
+}
 
 
+#end: none -> string string
+#marca el fin del proceso del archivo comprimir.
+end(){
+        echo "Fin de proceso de archivo comprimir"
+        echo -------------------------------------------------------
+}
 
+#main: none -> none
+#agrupa todas las funciones en el orden correspondiente
+main(){
+	init
+	getNames
+	getValidNames
+	endingCharA
+	compress
+	end
+}
+
+main
